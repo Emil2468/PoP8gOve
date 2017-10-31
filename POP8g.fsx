ac71d44550ce6bbcca131b56e8790d6b23f80dc7
impe98 <impe98@gmail.com>
2017-10-31 16:04:15 +0100

    8gø.0

--- POP8g.fsx
+++ POP8g.fsx
@@ -1,6 +1,22 @@
-/// POP8g Øvelsesopgaver
+// POP8g Øvelsesopgaver
 
 // 8gØ.0
 
-type weekday = Monday | Tuesday | Wendesday | Thursday | Friday | Saturday | Sunday
-let dayToNumber (weekday
+type weekday = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+
+let dayToNumber (day: weekday) : int =
+ match day with
+ | Monday -> 1
+ | Tuesday -> 2
+ | Wednesday -> 3
+ | Thursday -> 4
+ | Friday -> 5
+ | Saturday -> 6
+ | Sunday -> 7
+printfn "dayToNumber Monday = %A" (dayToNumber Monday)
+printfn "dayToNumber Tuesday = %A" (dayToNumber Tuesday)
+printfn "dayToNumber Wednesday = %A" (dayToNumber Wednesday)
+printfn "dayToNumber Thursday = %A" (dayToNumber Thursday)
+printfn "dayToNumber Friday = %A" (dayToNumber Friday)
+printfn "dayToNumber Saturday = %A" (dayToNumber Saturday)
+printfn "dayToNumber Sunday = %A" (dayToNumber Sunday)
---
+++ POP8g.fsx~
@@ -0,0 +1,6 @@
+/// POP8g Øvelsesopgaver
+
+// 8gØ.0
+
+type weekday = Monday | Tuesday | Wendesday | Thursday | Friday | Saturday | Sunday
+let dayToNumber (weekday
