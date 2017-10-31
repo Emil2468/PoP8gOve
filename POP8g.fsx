9a4335dec66fb0b21b9e513b2a89d69cc60deb0c
Emil2468 <emh@calaha.dk>
2017-10-31 16:14:00 +0100

    Fixed indentation

--- POP8g.fsx
+++ POP8g.fsx
@@ -5,14 +5,14 @@
 type weekday = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
 
 let dayToNumber (day: weekday) : int =
- match day with
- | Monday -> 1
- | Tuesday -> 2
- | Wednesday -> 3
- | Thursday -> 4
- | Friday -> 5
- | Saturday -> 6
- | Sunday -> 7
+    match day with
+    | Monday -> 1
+    | Tuesday -> 2
+    | Wednesday -> 3
+    | Thursday -> 4
+    | Friday -> 5
+    | Saturday -> 6
+    | Sunday -> 7
 printfn "dayToNumber Monday = %A" (dayToNumber Monday)
 printfn "dayToNumber Tuesday = %A" (dayToNumber Tuesday)
 printfn "dayToNumber Wednesday = %A" (dayToNumber Wednesday)
